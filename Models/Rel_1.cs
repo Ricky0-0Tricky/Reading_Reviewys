8f53aae04fb27f2cf9e74bd556ca9cff2c6e1299
Ricardo Gonçalves <yourmother_bel@hotmail.com>
2024-04-13 17:34:09 +0100

    Relacionamento entre Utilizador e Reviews

---
+++ Models/Rel_1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reading_Reviewys.Models {
+    public class Rel_1 {
+        // Definição da PK e FKs
+        [Key ,ForeignKey(nameof("Review"))]
+        public int? ReviewFK { get; set; }
+        public Reviews Review { get; set; }
+
+        [ForeignKey(nameof("Utilizador"))]
+        public int? UtilizadorFK { get; set; }
+        public Utilizador Utilizador { get; set; }
+    }
+}
