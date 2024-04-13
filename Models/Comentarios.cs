18ed9ab3240dececd78cb0e56a640c648ee06b3d
Ricardo Gonçalves <yourmother_bel@hotmail.com>
2024-04-13 17:13:21 +0100

    Relacionamento entre Reviews e Comentários

--- Models/Comentarios.cs
+++ Models/Comentarios.cs
@@ -1,9 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
+using System.Security.Cryptography.X509Certificates;
 
-namespace Reading_Reviewys.Models
-{
+namespace Reading_Reviewys.Models {
     public class Comentarios {
 
         [Key, Column(Order = 1)]
@@ -13,5 +13,14 @@ namespace Reading_Reviewys.Models
         public DateOnly Data { get; set;}
 
         public string Descricao { get; set;}
+
+        /*****************************
+         * Construção de Relacionamentos  
+         *****************************/
+
+        // relacionamento N-1 com Reviews
+        [ForeignKey(nameof(Review))]
+        public int ReviewFK { get; set;}
+        public Reviews Review { get; set;} 
     }
 }
--- Models/Reviews.cs
+++ Models/Reviews.cs
@@ -4,11 +4,24 @@ namespace Reading_Reviewys.Models
 {
     public class Reviews {
 
+        public Reviews() {
+            ListaComs = new HashSet<Comentarios>();
+        }
+
         [Key]
         public int IdReview { get; set;}
 
         public string DescricaoReview { get; set;}
 
-        public DateOnly DataAlteracao { get; set;} 
+        public DateOnly DataAlteracao { get; set;}
+
+        /*****************************
+         * Construção de Relacionamentos  
+         *****************************/
+
+        // relacionamento 1-N com Comentários
+
+        // Lista de Comentários que uma Review tem
+        public ICollection<Comentarios> ListaComs { get; set;}
     }
 }
